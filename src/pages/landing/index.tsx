1a0c0d58654abb0e844c3433fbaf71bf92dccedf
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-07-01 17:34:53 -0600

    feat: add landing page

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/layout';
+import React, { FC } from 'react';
+import Navbar from '@components/navbar/Navbar';
+
+const Landing: FC = () => (
+    <Layout>
+        <Navbar />
+    </Layout>
+);
+
+export default Landing;
