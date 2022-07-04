fd46cfc0c2eaf749d6e96e2cb8ce3b2f2b6cf51f
wmoralesdev <walterrafael26@gmail.com>
2022-07-03 21:53:24 -0600

    fix: temporary login btn in landing

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,9 +1,11 @@
 import Layout from '@components/layout';
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing: FC = () => (
     <Layout>
         <h1>Landing page</h1>
+        <Link to="/login">Login</Link>
     </Layout>
 );
 
