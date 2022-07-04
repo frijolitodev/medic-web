afefd553ec1c0d1f8ace94a19a2dee152fc8e8af
Alexander45448 <00098818@uca.edu.sv>
2022-07-03 21:30:38 -0600

    feat: added constants

---
+++ src/constants/time.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const longDateTimeFormat: Intl.DateTimeFormatOptions = {
+    weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
+};
+
+export const justTimeFormat: Intl.DateTimeFormatOptions = {
+    hour: '2-digit', minute: '2-digit',
+};
